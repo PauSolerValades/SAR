0c0a0178eaec77cf21731060fdf687ef3231087f
JonyyyV <jontxuuv@gmail.com>
2020-12-01 21:09:05 +0100

    Página de busqueda terminada, falta implementar los resultados y los PHP correspondientes. Recomendable comenzar con la pertinente documentación

--- practicageneral3/buscador.js
+++ practicageneral3/buscador.js
@@ -1,4 +1,8 @@
 sliderChange = function(n){
   var e = document.getElementById("valor")
-  e.innerHTML = n;
+  e.innerHTML = n + "€";
+}
+function botonPito(){
+  var e = document.getElementById("a");
+  e.addClass('active');
 }
